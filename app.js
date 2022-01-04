ba288c2a1704ff0c22868c934c142a7f131ed8fd
Danny <dgingerich14@gmail.com>
2022-01-04 03:23:33 +0000

    fixed app.config.json to run node app

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(helmet());
+
+app.use(cors());
+app.use(
+     helmet.contentSecurityPolicy({
+          useDefaults: true,
+          directives: {
+               defaultSrc: ["'self'", "https://fonts.googleapis.com", "dannygingerich.com:3333", "https://dannygingerich.com:3333", "http://dannygingerich.com:3333"],
+              // "font-src": ["'self'", "https://fonts.googleapis.com"],
+          }
+     })
+);
 
 
 const port = 3333;
